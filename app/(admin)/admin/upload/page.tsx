15c33274d4fde22e22453ca45629796df56041f9
0hyeon <djdjdjk2006@naver.com>
2024-05-17 02:30:36 +0900

    feat: 멀티업로드

--- app/(admin)/admin/upload/page.tsx
+++ app/(admin)/admin/upload/page.tsx
@@ -10,18 +10,18 @@ import { getUploadUrl, uploadProduct } from "./action";
 import Image from "next/image";
 
 export default function AddProduct() {
+  const [uploadUrl, setUploadUrl] = useState("");
   const [preview, setPreview] = useState("");
   const [file, setFile] = useState<File | null>(null);
-  const [uploadUrls, setUploadUrls] = useState<string>("");
 
   const [photoPreview, setPhotoPreview] = useState<string[]>([]);
-  const [slideUploadUrls, setSlideUploadUrls] = useState<string[]>([]);
-  const [slideFiles, setSlideFiles] = useState<File[]>([]);
-
+  const [slideUploadUrl, setSlideUploadUrl] = useState<string[]>([]);
+  const [slideFile, setSlideFile] = useState<File[]>([]);
   const {
     register,
     handleSubmit,
     setValue,
+    setError,
     formState: { errors },
   } = useForm<ProductType>({
     resolver: zodResolver(productSchema),
@@ -32,90 +32,113 @@ export default function AddProduct() {
     if (!files || files.length === 0) return;
 
     const file = files[0];
-    const url = URL.createObjectURL(file);
-    setPreview(url);
+    const previewurl = URL.createObjectURL(file);
+    setPreview(previewurl);
     setFile(file);
 
     const uploadResponse = await getUploadUrl();
     if (uploadResponse.success) {
       const { id, uploadURL } = uploadResponse.result;
-      setUploadUrls(uploadURL);
+      setUploadUrl(uploadURL);
       setValue(
         "photo",
         `https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${id}`
       );
     }
+    console.log(file)
   };
 
   const onSlideImageChange = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const files = Array.from(event.target.files || []);
-    if (files.length === 0) return;
-
-    const urls = files.map((file) => URL.createObjectURL(file));
-    setPhotoPreview(urls);
-    setSlideFiles(files);
-
-    for (const file of files) {
+    const files = event.target.files;
+    if (!files || files.length === 0) return;
+    const dummyid = []
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const previewUrl = URL.createObjectURL(file);
       const uploadResponse = await getUploadUrl();
       if (uploadResponse.success) {
         const { id, uploadURL } = uploadResponse.result;
-        setSlideUploadUrls((prev) => [...prev, uploadURL]);
+        dummyid.push(id)
+        setPhotoPreview((prev) => [...prev, previewUrl]);
+        setSlideUploadUrl((prev) => [...prev, uploadURL]);
+        setSlideFile((prev) => [...prev, file]);
       }
     }
+    setValue(
+      `photos`,
+      dummyid.map((id)=> `https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${id}`)
+      
+    );
   };
+  console.log(slideFile)
 
   const onSubmit = handleSubmit(async (data) => {
-    console.log("onSubmit");
+    
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      const response = await fetch(uploadUrl, {
+        method: "POST",
+        body: formData,
+      });
+      if (response.status !== 200) {
+      }
+    }
+    if(slideFile.length > 0){
+      for (let index = 0; index < slideFile.length; index++) {
+        const slideFormData = new FormData();
+        slideFormData.append("file", slideFile[index]);
+        const slideResponse = await fetch(slideUploadUrl[index], {
+          method: "POST",
+          body: slideFormData,
+        });
+        if (slideResponse.status !== 200) {
+          console.error(`Failed to upload slide image ${index}`);
+          continue
+        }
+      }
+
+    }
     const formData = new FormData();
     formData.append("title", data.title);
-    formData.append("price", data.price.toString());
+    formData.append("price", data.price + "");
     formData.append("description", data.description);
-
-    if (file) {
-      const response = await fetch(uploadUrls, {
-        method: "POST",
-        body: file,
-      });
-      if (response.ok) {
-        formData.append("photo", data.photo);
-      }
-    }
-
-    for (let i = 0; i < slideFiles.length; i++) {
-      const response = await fetch(slideUploadUrls[i], {
-        method: "POST",
-        body: slideFiles[i],
-      });
-      if (response.ok) {
-        formData.append("slidePhotos", slideFiles[i].name); // Modify this based on how you want to handle multiple image uploads on the server
-      }
-    }
-
+    formData.append("photo", data.photo);
+    console.log(data.photo)
     const errors = await uploadProduct(formData);
     if (errors) {
-      // setError logic here
-      console.log(errors);x
+      // setError("")
+      console.log(errors)
     }
+
   });
 
   const deleteHandler = (index: number) => {
     setPhotoPreview((prev) => prev.filter((_, idx) => idx !== index));
-    setSlideFiles((prev) => prev.filter((_, idx) => idx !== index));
-    setSlideUploadUrls((prev) => prev.filter((_, idx) => idx !== index));
+    setSlideFile((prev) => prev.filter((_, idx) => idx !== index));
+    setSlideUploadUrl((prev) => prev.filter((_, idx) => idx !== index));
   };
-
+  const onValid = async (e:any) => {
+    e.preventDefault();  
+    await onSubmit();
+  };
   return (
     <div className="w-1/4 mx-auto my-10">
-      <form onSubmit={onSubmit} className="p-5 flex flex-col gap-5">
+      <form onSubmit={onValid} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
           style={{ backgroundImage: `url(${preview})` }}
         >
-          {preview === "" ? <PhotoIcon className="w-20" /> : null}
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+            </>
+          ) : null}
         </label>
+
         <input
           onChange={onImageChange}
           type="file"
@@ -134,7 +157,7 @@ export default function AddProduct() {
             aria-hidden="true"
           >
             <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172 a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
               strokeWidth={2}
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -150,27 +173,28 @@ export default function AddProduct() {
             multiple
           />
         </label>
-        {photoPreview.length > 0 && (
-          <div className="flex w-full overflow-x-scroll">
-            {photoPreview.map((src, idx) => (
+        {photoPreview ? (
+          <div className="flex w-30">
+            {photoPreview.map((src: any, idx: any) => (
               <span className="mr-3 relative" key={idx}>
                 <span
                   className="cursor-pointer absolute z-10 right-[-5px] top-[-10px] bg-black text-white rounded-full w-5 h-5 flex justify-center items-center text-sm"
                   onClick={() => deleteHandler(idx)}
+                  key={idx}
                 >
                   X
                 </span>
                 <Image
                   src={src}
-                  className="rounded-md bg-slate-300 object-cover"
+                  className=" text-gray-600 h-46 rounded-md bg-slate-300 object-cover"
                   width={80}
                   height={80}
-                  alt="Slide Image"
+                  alt={src}
                 />
               </span>
             ))}
           </div>
-        )}
+        ) : null}
 
         <Input
           required
--- app/(admin)/admin/upload/shema.ts
+++ app/(admin)/admin/upload/shema.ts
@@ -1,14 +1,12 @@
 import { z } from "zod";
 
 export const productSchema = z.object({
-  slidePhotos: z.string({
-    required_error: "Photo is required",
-  }),
+  photos: z.array(z.string()).min(1, "At least one photo is required"),
   photo: z.string({
-    required_error: "Photo is required",
+    required_error: "photo is required",
   }),
   title: z.string({
-    required_error: "Title is required!!!!!",
+    required_error: "Title is required",
   }),
   description: z.string({
     required_error: "Description is required",
