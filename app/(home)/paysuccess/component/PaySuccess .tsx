ef3184a7e87cf52b8476f639392fc9445e4c6468
0hyeon <djdjdjk2006@naver.com>
2024-10-24 02:49:48 +0900

    fix: web hook

--- app/(home)/paysuccess/component/PaySuccess .tsx
+++ app/(home)/paysuccess/component/PaySuccess .tsx
@@ -1,38 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { handlePaymentVerification } from "@/lib/hooks/usePaymenUtil";
-import { triggerConfetti } from "@/lib/hooks";
 
 interface IStatus {
   amount: number;
-  balanceAmt: number;
-  buyerEmail: string;
-  currency: string;
-  ediDate: string;
-  failedAt: string;
-  goodsName: string;
-  issuedCashReceipt: boolean;
-  mallReserved: string;
-  messageSource: string;
-  orderId: string;
-  paidAt: string;
-  payMethod: string;
-  receiptUrl: string;
-  resultCode: string;
-  resultMsg: string;
-  signature: string;
   status: string;
-  tid: string;
-  message: string;
-  useEscrow: boolean;
-  vbank: {
-    vbankCode: string;
-    vbankExpDate: string;
-    vbankHolder: string;
-    vbankName: string;
-    vbankNumber: string;
-  };
 }
 
 export default function PaySuccess() {
@@ -40,25 +12,13 @@ export default function PaySuccess() {
 
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
-    const orderId = query.get("orderId");
     const amount = Number(query.get("amount"));
-    const tid = query.get("tid");
-    const verifyPayment = async () => {
-      if (orderId && amount && tid) {
-        const verificationResult = await handlePaymentVerification(
-          orderId,
-          amount,
-          tid
-        );
-        // console.log("verificationResult : ", verificationResult);
-        setStatusData(verificationResult as IStatus);
-        if (verificationResult.status === "paid") {
-          triggerConfetti();
-        }
-      }
-    };
+    const status = query.get("status");
 
-    verifyPayment();
+    setStatusData({
+      amount: amount,
+      status: status || "unknown",
+    });
   }, []);
 
   if (statusData === null) return <div>로딩 중...</div>;
@@ -68,24 +28,20 @@ export default function PaySuccess() {
       {statusData.status === "paid" ? (
         <div className="text-center text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="text-2xl mb-4">구매가 완료되었습니다.</div>
+          <div>결제 금액: {statusData.amount}원</div>
           <div>이용해주셔서 감사합니다.</div>
         </div>
       ) : (
-        <div>{statusData.message}</div>
+        <div>결제가 실패했습니다. 다시 시도해 주세요.</div>
       )}
       {statusData.status === "ready" ? (
         <div className="text-center text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="text-2xl mb-4">
-            가상계좌 입금대기입니다 문자발송 {statusData.resultMsg}
+            가상계좌 입금 완료 문자를 발송하였습니다.
           </div>
-          <div>상품명 : {statusData.goodsName}</div>
-          <div>금액 : {statusData.amount}원</div>
-          <div>은행명 : {statusData.vbank.vbankName}</div>
-          <div>예금주명 : {statusData.vbank.vbankHolder}</div>
-          <div>입금계좌 : {statusData.vbank.vbankNumber}</div>
         </div>
       ) : (
-        <div>{statusData.message}</div>
+        <div>결제가 실패했습니다. 다시 시도해 주세요.</div>
       )}
     </div>
   );
--- app/api/serverAuth/route.ts
+++ app/api/serverAuth/route.ts
@@ -1,95 +1,62 @@
-import { updateCart } from "@/app/(home)/cart/actions";
-import { NextResponse } from "next/server";
-import qs from "qs"; // query string 파싱을 위한 라이브러리
+import { NextRequest, NextResponse } from "next/server";
+import crypto from "crypto";
 
-export async function POST(request: Request) {
-  const bodyText = await request.text();
+function generateSignData(
+  tid: string,
+  amount: string,
+  ediDate: string,
+  secretKey: string
+) {
+  const data = `${tid}${amount}${ediDate}${secretKey}`;
+  return crypto.createHash("sha256").update(data).digest("hex");
+}
 
-  // URL-encoded 데이터를 파싱합니다
-  const body = qs.parse(bodyText);
-  console.log("serverAuth : ", body);
-  // serverAuth :  {
-  //   authResultCode: '0000',
-  //   authResultMsg: '인증 성공',
-  //   tid: 'UT0013870m03012410192304232121',
-  //   clientId: 'S2_07a6c2d843654d7eb32a6fcc0759eef4',
-  //   orderId: '1729346637375425',
-  //   amount: '30000',
-  //   mallReserved: '{"phoneNumber":"01041096590","cartIds":"29"}',
-  //   authToken: 'NICEUNTT7E6FA672A8A1420665A8B29D86DED574',
-  //   signature: '9882b635be8b2a6726888b95e2dd3ae0c971222b553409a2a837a9cab562cbff'
-  // }
-  const { authResultCode, tid, orderId, amount, mallReserved, authResultMsg } =
-    body;
+export async function POST(request: NextRequest) {
+  const { orderId, amount, tid } = await request.json();
+  const clientKey = "R2_8bad4063b9a942668b156d221c3489ea"; // 실제 운영 키
+  const secretKey = "731f20c8498345b1ba7db90194076451"; // 실제 운영 시크릿 키
+
+  const authHeader =
+    "Basic " + Buffer.from(`${clientKey}:${secretKey}`).toString("base64");
+  const ediDate = new Date().toISOString();
+  const signData = generateSignData(tid, String(amount), ediDate, secretKey);
 
   try {
-    // mallReserved가 문자열이므로 JSON으로 파싱합니다.
-    const parsedMallReserved = JSON.parse(mallReserved as string);
-    const phoneNumber = parsedMallReserved?.phoneNumber || null;
-    const cartIds = parsedMallReserved?.cartIds || null;
-
-    if (typeof orderId === "string" && cartIds) {
-      const [originalOrderId] = orderId.split("-");
-
-      if (authResultCode === "0000") {
-        console.log("인증 성공:", tid);
-        console.log("authResultMsg : ", authResultMsg);
-
-        if (authResultMsg === "인증 성공") {
-          const redirectUrl =
-            process.env.NODE_ENV === "production"
-              ? `https://aurorafactory.vercel.app/paysuccess?orderId=${orderId}&amount=${amount}&tid=${tid}`
-              : `http://localhost:3000/paysuccess?orderId=${orderId}&amount=${amount}&tid=${tid}`;
-
-          return NextResponse.redirect(redirectUrl);
-        } else {
-          // 결제 인증이 아닌 경우 장바구니 업데이트
-          const result = await updateCart({
-            cartIds: cartIds.split("-").map(Number),
-            orderId: originalOrderId,
-          });
-          if (result.success) {
-            const redirectUrl =
-              process.env.NODE_ENV === "production"
-                ? `https://aurorafactory.vercel.app/paysuccess?orderId=${orderId}&amount=${amount}&tid=${tid}`
-                : `http://localhost:3000/paysuccess?orderId=${orderId}&amount=${amount}&tid=${tid}`;
-
-            return NextResponse.redirect(redirectUrl);
-          } else {
-            return NextResponse.json(
-              {
-                status: "failed",
-                message: "장바구니 업데이트에 실패하였습니다.",
-              },
-              { status: 400 }
-            );
-          }
-        }
-      } else {
-        console.log("인증 실패:", authResultCode);
-        return NextResponse.json(
-          { status: "failed", message: "결제 인증에 실패하였습니다." },
-          { status: 400 }
-        );
-      }
-    } else {
-      console.error("cartIds 또는 orderId가 유효하지 않습니다.");
-      return NextResponse.json(
-        {
-          status: "failed",
-          message: "유효한 cartIds 또는 orderId가 없습니다.",
+    // Nicepay 결제 승인 API 호출
+    const response = await fetch(
+      `https://api.nicepay.co.kr/v1/payments/${tid}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: authHeader,
         },
-        { status: 400 }
-      );
+        body: JSON.stringify({
+          amount: String(amount),
+          ediDate: ediDate,
+          signData: signData,
+        }),
+      }
+    );
+
+    const responseBody = await response.json();
+    console.log("serverAuth : ", responseBody);
+    if (responseBody.resultCode === "0000") {
+      // 승인 성공 시, PaySuccess 페이지로 승인 결과 전달
+      const redirectUrl = `https://aurorafactory.vercel.app/paysuccess?orderId=${orderId}&amount=${amount}&status=paid`;
+      return NextResponse.redirect(redirectUrl);
+    } else {
+      return NextResponse.json({
+        status: "failed",
+        message: responseBody.resultMsg,
+      });
     }
   } catch (error) {
-    console.error("mallReserved 파싱 중 오류 발생:", error);
-    return NextResponse.json(
-      {
-        status: "failed",
-        message: "mallReserved 파싱 중 오류가 발생했습니다.",
-      },
-      { status: 500 }
-    );
+    console.error("Error:", error);
+    return NextResponse.json({
+      status: "error",
+      message: "Server error",
+      error: error,
+    });
   }
 }
